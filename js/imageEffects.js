1f83087153b6fb8c677f8a7018e1ebe2277ff52b
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-03-20 10:43:56 +0300

    Внес окончательные доработки функции и добавил свитч вместо условий

--- js/imageEffects.js
+++ js/imageEffects.js
@@ -16,20 +16,22 @@ const uiSlider = noUiSlider.create(slider, {
 let selectedType = '';
 uiSlider.on('update', (values) => {
   effectLevelValue.value = +values[0];
-  if(selectedType === 'chrome') {
-    imageUploadPreview.style.filter = `grayscale(${values[0]})`;
-  }
-  if(selectedType === 'sepia') {
-    imageUploadPreview.style.filter = `sepia(${values[0]})`;
-  }
-  if(selectedType === 'marvin') {
-    imageUploadPreview.style.filter = `invert(${values[0]}%)`;
-  }
-  if(selectedType === 'phobos') {
-    imageUploadPreview.style.filter = `blur(${values[0]}px)`;
-  }
-  if(selectedType === 'heat') {
-    imageUploadPreview.style.filter = `brightness(${values[0]})`;
+  switch(selectedType) {
+    case 'chrome':
+      imageUploadPreview.style.filter = `grayscale(${values[0]})`;
+      break;
+    case 'sepia':
+      imageUploadPreview.style.filter = `sepia(${values[0]})`;
+      break;
+    case 'marvin':
+      imageUploadPreview.style.filter = `invert(${values[0]}%)`;
+      break;
+    case 'phobos':
+      imageUploadPreview.style.filter = `blur(${values[0]}px)`;
+      break;
+    case 'heat':
+      imageUploadPreview.style.filter = `brightness(${values[0]})`;
+      break;
   }
 });
 
@@ -49,7 +51,8 @@ const onChangeUploadInputSetDefaultEffect = () => {
 };
 imageUploadInput.addEventListener('change', onChangeUploadInputSetDefaultEffect);
 
-const changeUiSlider = (min, max, step) => {
+const changeUiSlider = (min, max, step, type) => {
+  selectedType = type;
   uiSlider.updateOptions({
     range: {
       min: min,
@@ -62,31 +65,26 @@ const changeUiSlider = (min, max, step) => {
 efffectDefault.addEventListener('click', onChangeUploadInputSetDefaultEffect);
 const addChromeEffect = () => {
   effectLevel.classList.remove('hidden');
-  selectedType = 'chrome';
-  changeUiSlider(0, 1, 0.1);
+  changeUiSlider(0, 1, 0.1, 'chrome');
 };
 effectChrome.addEventListener('click', addChromeEffect);
 const addSepiaEffect = () => {
   effectLevel.classList.remove('hidden');
-  selectedType = 'sepia';
-  changeUiSlider(0, 1, 0.1);
+  changeUiSlider(0, 1, 0.1, 'sepia');
 };
 effectSepia.addEventListener('click', addSepiaEffect);
 const addMarvinEffect = () => {
   effectLevel.classList.remove('hidden');
-  selectedType = 'marvin';
-  changeUiSlider(0, 100, 1);
+  changeUiSlider(0, 100, 1, 'marvin');
 };
 effectMarvin.addEventListener('click', addMarvinEffect);
 const addPhobosEffect = () => {
   effectLevel.classList.remove('hidden');
-  selectedType = 'phobos';
-  changeUiSlider(0, 3, 0.1);
+  changeUiSlider(0, 3, 0.1, 'phobos');
 };
 effectPhobos.addEventListener('click', addPhobosEffect);
 const addHeatEffect = () => {
   effectLevel.classList.remove('hidden');
-  selectedType = 'heat';
-  changeUiSlider(1, 3, 0.1);
+  changeUiSlider(1, 3, 0.1, 'heat');
 };
 effectHeat.addEventListener('click', addHeatEffect);
