1218a063ff4a1e25e000959b3d6a2008dacb7784
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-03-30 11:23:30 +0300

    Доработал фильтер

--- js/filter.js
+++ js/filter.js
@@ -6,42 +6,44 @@ const COUNT_RANDOM_IMAGE = 10;
 const initFilter = (photos) => {
   const filters = document.querySelector('.img-filters');
   filters.classList.remove('img-filters--inactive');
-  const filterDefault = document.querySelector('#filter-default');
-  const filterRandom = document.querySelector('#filter-random');
-  const filterDiscussed = document.querySelector('#filter-discussed');
-  const filterButtons = document.querySelectorAll('.img-filters__button');
+  const filterForm = document.querySelector('.img-filters__form');
+
   const onClickDefaultFilter = () => {
-    for(let i = 0; i < filterButtons.length; i++) {
-      filterButtons[i].classList.remove('img-filters__button--active');
-    }
-    filterDefault.classList.add('img-filters__button--active');
     showPictures(photos);
   };
-  const onClickDefaultFilterDebounce = debounce(onClickDefaultFilter);
-  filterDefault.addEventListener('click', onClickDefaultFilterDebounce);
   const onClickRandomFilter = () => {
-    for(let i = 0; i < filterButtons.length; i++) {
-      filterButtons[i].classList.remove('img-filters__button--active');
-    }
-    filterRandom.classList.add('img-filters__button--active');
     let copyPhotos = photos.slice();
     copyPhotos.sort(() => 0.5 - Math.random());
     copyPhotos = copyPhotos.slice(0, COUNT_RANDOM_IMAGE);
     showPictures(copyPhotos);
   };
-  const onClickRandomFilterDebounce = debounce(onClickRandomFilter);
-  filterRandom.addEventListener('click', onClickRandomFilterDebounce);
   const onClickDiscussedFilter = () => {
-    for(let i = 0; i < filterButtons.length; i++) {
-      filterButtons[i].classList.remove('img-filters__button--active');
-    }
-    filterDiscussed.classList.add('img-filters__button--active');
     const copyPhotos = photos.slice();
     copyPhotos.sort((photo1, photo2) => photo2.comments.length - photo1.comments.length);
     showPictures(copyPhotos);
   };
-  const onClickDiscussedFilterDebounce = debounce(onClickDiscussedFilter);
-  filterDiscussed.addEventListener('click', onClickDiscussedFilterDebounce);
+
+  const onClickFilter = (evt) => {
+    if(!evt.target.closest('.img-filters__button')) {
+      return;
+    }
+    const checkedEl = document.querySelector('.img-filters__button--active');
+    checkedEl.classList.remove('img-filters__button--active');
+    const elId = evt.target.id;
+    evt.target.classList.add('img-filters__button--active');
+    switch(elId) {
+      case 'filter-default':
+        onClickDefaultFilter();
+        break;
+      case 'filter-random':
+        onClickRandomFilter();
+        break;
+      case 'filter-discussed':
+        onClickDiscussedFilter();
+        break;
+    }
+  };
+  filterForm.addEventListener('click', debounce(onClickFilter));
 };
 
 export {initFilter};
